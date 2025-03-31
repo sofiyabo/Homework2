d407b52dab171edf20cb9729a83d306f3bb914d3
Sofi Yabo <syabo@udesa.edu.ar>
2025-03-31 15:11:42 -0300

    principio menues ej 1

--- Ejercicio1/ej1.cpp
+++ Ejercicio1/ej1.cpp
@@ -6,8 +6,9 @@
 #include <iostream>
 #include <iomanip>
 
-
+/*
 int main(){
+
     //1.a
     Reloj tiempo_a;
     tiempo_a.imprimir();
@@ -61,5 +62,71 @@ int main(){
     
     return 0;
 }
+    */
 
 //HACER LOS DOS MENUES; como se quiere inicializar y el de modificaciones
+
+int main(){
+    while(true){
+        int opcion;
+        int hh, mm, ss = 0;
+        
+        try{
+            std::cout<< "Cómo quiere inicializar el tiempo?\n1. Default\n2. Horas\n3. Horas y minutos\n4. Horas, minutos y segundos.\n5. Horas, minutos, segundos y formato."<< std::endl;
+            std::cin>>opcion;
+
+            if(opcion == 1){
+                Reloj tiempo;
+
+            }
+            else if(opcion == 2){
+                std::cout << "Introduzca las horas (0 - 12): ";
+                std::cin >> hh;
+                
+                Reloj tiempo(hh);
+            }
+            else if(opcion == 3){
+                std::cout << "Introduzca las horas (0 - 12): ";
+                std::cin >> hh;
+
+                std::cout << "Introduzca los minutos (0 - 59): ";
+                std::cin >> mm;
+                
+                Reloj tiempo(hh, mm);
+            }
+            else if(opcion == 4){
+                std::cout << "Introduzca las horas (0 - 12): ";
+                std::cin >> hh;
+
+                std::cout << "Introduzca los minutos (0 - 59): ";
+                std::cin >> mm;
+
+                std::cout << "Introduzca los segundos (0 - 59): ";
+                std::cin >> ss;
+                
+                Reloj tiempo(hh, mm, ss);
+            }
+            else if(opcion == 5){
+            std::string formato;
+            
+            std::cout << "Introduzca las horas (0 - 12): ";
+            std::cin >> hh;
+
+            std::cout << "Introduzca los minutos (0 - 59): ";
+            std::cin >> mm;
+
+            std::cout << "Introduzca los segundos (0 - 59): ";
+            std::cin >> ss;
+
+            std::cout << "Introduzca los el formato (a.m / p.m)";
+            std::cin >> formato;
+            
+            Reloj tiempo(hh, mm, ss, formato);
+        }
+
+    }catch(const std::exception& e){
+        std::cout << "Error " << e.what() << std::endl;}
+
+        
+    }
+}
--- Ejercicio2/Curso.hpp
+++ Ejercicio2/Curso.hpp
@@ -3,15 +3,21 @@
 #include <vector>
 #include <memory>
 
+//Capacidad maxima de estudiantes es 20
 class Curso
 {
 private:
      std::vector<std::shared_ptr<Estudiante>> lista_estudiantes;
+     int cantidad_estudiantes;
     
 public:
     Curso();
     void inscribir_estudiante(Estudiante);
-    
+    void desincribir_estudiante(int num_legajo);
+    void buscar_estudiante(int num_legajo);
+    void capacidad_curso();
+    void imprimir_orden_alfabetico();
+    //Copia objeto curso
 
 };
 
