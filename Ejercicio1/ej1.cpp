81d2cbdc64f306aa54e2844700f202b7c26e5cbd
Sofi Yabo <syabo@udesa.edu.ar>
2025-04-01 16:17:38 -0300

    menu ej1 terminado

--- Ejercicio1/ej1.cpp
+++ Ejercicio1/ej1.cpp
@@ -6,107 +6,46 @@
 #include <iostream>
 #include <iomanip>
 
-/*
+
 int main(){
+    
+    int opcion;
+    int hh, mm, ss = 0;
+    
+    Reloj tiempo;
 
-    //1.a
-    Reloj tiempo_a;
-    tiempo_a.imprimir();
+    try{
+        std::cout<< "Cómo quiere inicializar el tiempo?\n1. Default\n2. Horas\n3. Horas y minutos\n4. Horas, minutos y segundos.\n5. Horas, minutos, segundos y formato."<< std::endl;
+        std::cin>>opcion;
 
-    //1.b
-    Reloj tiempo_b(4);
-    tiempo_b.imprimir();
-
-    //1.c
-    Reloj tiempo_c(5, 35);
-    tiempo_c.imprimir();
-
-    //1.d
-    Reloj tiempo_d(7, 50, 45);
-    tiempo_d.imprimir();
-
-    //1.e
-    Reloj tiempo_e(9, 30, 21, "p.m");
-    tiempo_e.imprimir();
-
-    //1.f
-    //Reloj tiempo_f(30);
-    //COMPLETAR
-
-    //1.i
-    Reloj tiempo_i;
-    int hh, mm, ss;
-    std::string formato;
-
-    while(true){
-        try{
+        if(opcion == 2){
             std::cout << "Introduzca las horas (0 - 12): ";
             std::cin >> hh;
-            tiempo_i.set_horas(hh);
-    
-            std::cout << "Introduce los minutos (0-59): ";
+                
+            tiempo = Reloj(hh);
+            }
+        else if(opcion == 3){
+            std::cout << "Introduzca las horas (0 - 12): ";
+            std::cin >> hh;
+
+            std::cout << "Introduzca los minutos (0 - 59): ";
             std::cin >> mm;
-            tiempo_i.set_minutos(mm);
-    
-            std::cout << "Introduce los segundos (0-59): ";
+                
+            tiempo = Reloj(hh, mm);
+            }
+        else if(opcion == 4){
+            std::cout << "Introduzca las horas (0 - 12): ";
+            std::cin >> hh;
+
+            std::cout << "Introduzca los minutos (0 - 59): ";
+            std::cin >> mm;
+
+            std::cout << "Introduzca los segundos (0 - 59): ";
             std::cin >> ss;
-            tiempo_i.set_segundos(ss);
-    
-            std::cout << "Introduce el formato (a.m. o p.m.): ";
-            std::cin >> formato;
-            tiempo_i.set_formato(formato);
-        } catch (const std::exception& e){
-            std::cout << "Error " << e.what() << std::endl;
-        }
-    }
-    
-    return 0;
-}
-    */
-
-//HACER LOS DOS MENUES; como se quiere inicializar y el de modificaciones
-
-int main(){
-    while(true){
-        int opcion;
-        int hh, mm, ss = 0;
-        
-        try{
-            std::cout<< "Cómo quiere inicializar el tiempo?\n1. Default\n2. Horas\n3. Horas y minutos\n4. Horas, minutos y segundos.\n5. Horas, minutos, segundos y formato."<< std::endl;
-            std::cin>>opcion;
-
-            if(opcion == 1){
-                Reloj tiempo;
-
-            }
-            else if(opcion == 2){
-                std::cout << "Introduzca las horas (0 - 12): ";
-                std::cin >> hh;
                 
-                Reloj tiempo(hh);
+            tiempo = Reloj(hh, mm, ss);
             }
-            else if(opcion == 3){
-                std::cout << "Introduzca las horas (0 - 12): ";
-                std::cin >> hh;
-
-                std::cout << "Introduzca los minutos (0 - 59): ";
-                std::cin >> mm;
-                
-                Reloj tiempo(hh, mm);
-            }
-            else if(opcion == 4){
-                std::cout << "Introduzca las horas (0 - 12): ";
-                std::cin >> hh;
-
-                std::cout << "Introduzca los minutos (0 - 59): ";
-                std::cin >> mm;
-
-                std::cout << "Introduzca los segundos (0 - 59): ";
-                std::cin >> ss;
-                
-                Reloj tiempo(hh, mm, ss);
-            }
-            else if(opcion == 5){
+        else if(opcion == 5){
             std::string formato;
             
             std::cout << "Introduzca las horas (0 - 12): ";
@@ -121,12 +60,62 @@ int main(){
             std::cout << "Introduzca los el formato (a.m / p.m)";
             std::cin >> formato;
             
-            Reloj tiempo(hh, mm, ss, formato);
+            tiempo = Reloj(hh, mm, ss, formato);
         }
 
-    }catch(const std::exception& e){
-        std::cout << "Error " << e.what() << std::endl;}
+        }catch(const std::exception& e){
+            std::cout <<  e.what() << std::endl;}
+    
+    
+    while(true){
+        int continuar;
 
-        
+        std::cout << "Como desea continuar?\n1. Mostrar reloj\n2. Cambiar hora\n3. Cambiar minutos\n4. Cambiar segundos\n5. Cambiar formato\n6. Salir"<< std::endl;
+        std::cin >> continuar;
+
+        if(continuar == 1){
+            int eleccion;
+
+            std::cout<< "En que formato desea ver el reloj?\n1. Formato 0-12 horas\n2. Formato 24 horas."<< std::endl;
+            std::cin >> eleccion;
+            if(eleccion == 1){
+                tiempo.imprimir();
+            }
+            else{
+                tiempo.imprimir_24hs();
+            }
+        }
+        else if(continuar == 2){
+            int horas;
+
+            std::cout << "Ingrese las horas:"<< std::endl;
+            std::cin >> horas;
+            tiempo.set_horas(horas);
+        }
+        else if(continuar == 3){
+            int minutos;
+
+            std::cout << "Ingrese los minutos:"<< std::endl;
+            std::cin >> minutos;
+            tiempo.set_minutos(minutos);
+        }
+        else if(continuar == 4){
+            int segundos;
+
+            std::cout << "Ingrese los segundos:"<< std::endl;
+            std::cin >> segundos;
+            tiempo.set_segundos(segundos);
+        }
+        else if(continuar == 5){
+            std::string formato;
+
+            std::cout << "Ingrese el formato (a.m / p.m):"<< std::endl;
+            std::cin >> formato;
+            tiempo.set_formato(formato);
+        }
+        else if(continuar == 6){
+            return 0;
+        }
+
     }
 }
