4f8f56c55b2987f183172b0e9f6ee0167fef072e
Sofi Yabo <syabo@udesa.edu.ar>
2025-04-02 23:07:34 -0300

    comienzo header ej4

--- Ejercicio4/header1.hpp
+++ Ejercicio4/header1.hpp
@@ -4,3 +4,22 @@
 //metodo virtual: puede tener una implementacion en la clase base.
 //no es obligatorio sobreescribirlo en la clase base.
 
+#include <iostream>
+
+class Cuenta_Bancaria{ //clase abstracta
+    public:
+        double balance = 0;
+        std::string titularCuenta = 0;
+        virtual void depositar(double cantidad) = 0;
+        virtual void retirar(double cantidad) = 0;
+        virtual void mostrarInfo() = 0;
+
+};
+
+class CajaDeAhorro : public Cuenta_Bancaria{
+
+};
+
+class CuentaCorriente : public Cuenta_Bancaria{
+    
+};
